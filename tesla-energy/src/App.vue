83bff8d5445833a8a1d69c71387044ad3e10ada4
Camilo Sanchez <juan.barreiro@correounivalle.edu.co>
2020-02-26 23:18:47 -0500

    agrego formulario de Login

--- tesla-energy/src/App.vue
+++ tesla-energy/src/App.vue
@@ -1,12 +1,78 @@
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+<v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      fixed
+      temporary
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Tesla Energy</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <router-view></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; Tesla Energy 2020</span>
+    </v-footer>
+  </v-app>
+</div>
 </template>
+
+<script>
+// import login from './components/Login'
+export default {
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
 
 <style>
 #app {
---
+++ tesla-energy/src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-app id="inspire">
+      <!-- <v-content> -->
+        <v-container class="text-xs-center" style="min-height: 100vh" fluid>
+          <v-layout align-center justify-center >
+            <v-flex xs12 sm10 md6 >
+              <v-card class="elevation-20" >
+                <v-toolbar dark color="primary">
+                  <v-toolbar-title>Login</v-toolbar-title>
+                  <v-spacer></v-spacer>
+                </v-toolbar>
+                <v-card-text>
+                  <v-form @submit.prevent="login()">
+                    <v-text-field v-model="user.cedula" prepend-icon="person" name="login" label="Login" type="text" required></v-text-field>
+                    <v-text-field v-model="user.password" prepend-icon="lock" name="password" label="Password" id="password" type="password" required></v-text-field>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn type="submit" color="pink">Login</v-btn>
+                    </v-card-actions>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      <!-- </v-content> -->
+    </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: {
+        cedula: null,
+        password: null
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$store.dispatch('login', this.user)
+        .then(response => {
+          this.success = true
+          this.$router.push({ path: '/' })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
