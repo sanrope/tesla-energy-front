be3c7000add691e7775c4811b231defba4f37fc5
SantiagoRP7 <santiago.19988@gmail.com>
2020-04-02 16:22:25 -0500

    update users finished, and some bugs improved

--- tesla-energy/src/components/Users.vue
+++ tesla-energy/src/components/Users.vue
@@ -18,43 +18,6 @@
         :items="users"
         :search="search"
         >
-          <v-dialog v-slot:top v-model="dialog" max-width="500px">
-            <v-card>
-              <v-card-title>
-                <span class="headline">One Title</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.username" label="first name"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.first_name" label="last name"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.last_name" label="username (g)"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.email" label="email (g)"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.is_active" label="active (g)"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.rol" label="rol"></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
           <template v-slot:item.actions="{ item }">
             <v-icon
               small
@@ -71,7 +34,42 @@
             </v-icon>
           </template>
         </v-data-table>
-        <v-btn @click="getUsers" >GET</v-btn>
+        <v-dialog v-model="dialog" max-width="500px">
+            <v-card>
+              <v-card-title>
+                <span class="headline">One Title</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.first_name" label="first name"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.last_name" label="last name"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field :disabled="true" v-model="editedItem.username" label="username"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.email" label="email"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-select :items="items_box" v-model="editedItem.is_active" label="active"></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.rol" label="rol"></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
       </v-card>
     </v-col>
   </v-row>
@@ -83,7 +81,7 @@ export default {
   data () {
     return {
       search: '',
-      dialog: true,
+      dialog: false,
       headers: [
         { text: 'Username', value: 'username' },
         { text: 'Name', value: 'first_name' },
@@ -100,8 +98,14 @@ export default {
         email: '',
         is_active: '',
         rol: ''
-      }
-      // users: []
+      },
+      items_box: [
+        {
+          text: 'activo', value: true
+        },
+        {
+          text: 'inactivo', value: false
+        }]
     }
   },
   watch: {
@@ -117,16 +121,30 @@ export default {
         })
     },
     editUser (item) {
-      console.log(item)
-      this.editedItem = Object.assign({}, item)
       this.dialog = true
+      this.editedItem = Object.assign({}, item)
     },
     deleteUser (item) {
-      console.log(item)
+      this.editedItem = Object.assign({}, item)
+      this.editedItem.is_active = false
+      this.save()
     },
     close () {
+      this.dialog = false
     },
-    save () {
+    async save () {
+      var newUser = {
+        username: this.editedItem.username,
+        password: this.editedItem.password,
+        first_name: this.editedItem.first_name,
+        last_name: this.editedItem.last_name,
+        email: this.editedItem.email,
+        rol: this.editedItem.rol,
+        is_active: this.editedItem.is_active
+      }
+      await this.$store.dispatch('updateUser', newUser)
+      this.$store.dispatch('obtainUsers')
+      this.close()
     }
   },
   computed: {
