3e858460efd70e0970f168bcd8cada552b2fcb7f
SantiagoRP7 <santiago.19988@gmail.com>
2020-05-13 16:35:47 -0500

    Create substation, Create transformers and Create Electricmeter finished

--- tesla-energy/src/components/Map.vue
+++ tesla-energy/src/components/Map.vue
@@ -10,6 +10,7 @@
         style="z-index: 1"
       >
         <l-map
+          ref="Map"
           :zoom="zoom"
           :center="center"
           :options="mapOptions"
@@ -26,13 +27,14 @@
           :icon="iconSubstation"
           :lat-lng="latLng2(substation.latitude, substation.longitude)"
           >
+          <l-tooltip>{{substation.name}}</l-tooltip>
             <l-popup>
               <v-row>
                 <v-col cols="12">
                   <v-menu center>
                     <template v-slot:activator="{ on }">
                       <div v-on="on">
-                      Substation, Transformer or Electric Meter: {{substation.name}}
+                      Substation: {{substation.name}}
                         <p v-show="showParagraph">
                           Latitude:  {{substation.latitude}}<br>
                           Longitude:  {{substation.longitude}}
@@ -54,34 +56,30 @@
           :icon="iconTransformator"
           :lat-lng="latLng2(transformer.latitude, transformer.longitude)"
           >
-            <l-popup>
-              <v-row>
+          <l-tooltip>{{transformer.name}}</l-tooltip>
+            <l-popup >
+                <v-card width="15vw">
+              <v-container>
+            <v-form>
                 <v-col cols="12">
-                  <v-menu center>
-                    <template v-slot:activator="{ en }">
-                      <div v-on="en">
-                      Substation, Transformer or Electric Meter: {{transformer.name}}
-                        <p v-show="showParagraph">
-                          Latitude:  {{transformer.latitude}}<br>
-                          Longitude:  {{transformer.longitude}}<br>
-                          substation-id:  {{transformer.substation}}
-                        </p>
-                      </div>
-                    </template>
-                    <v-list>
-                      <v-list-item>
-                        <v-list-item-title>one item menu</v-list-item-title>
-                      </v-list-item>
-                    </v-list>
-                  </v-menu>
-                </v-col>
+              <v-row>
+                  <v-text-field
+                  label="Name"></v-text-field>
               </v-row>
+              <v-row>
+                <v-text-field
+                label="Pos"></v-text-field>
+              </v-row>
+                </v-col>
+            </v-form>
+              </v-container>
+                </v-card>
             </l-popup>
           </l-marker>
 <!-- Marker drawing when a point on map its selected -->
           <l-marker
           ref="ClickMarker"
-          :icon="active_type === 'S' ? iconSubstation : active_type === 'T' ? iconTransformator : active_type === 'E' ? iconEMeter : null"
+          :icon="ClickMarkerIcon"
           width="30vw"
           :visible="positionPopup ? true : false"
           :lat-lng="positionPopup"
@@ -94,6 +92,7 @@
                     <v-col cols="12">
                       <v-row>
                     <v-select outlined
+                    v-on:change="changeMarkerIcon"
                      label="Active type"
                      :items="actives"
                      v-model="active_type"
@@ -132,7 +131,7 @@
 <script>
 /* eslint-disable */
 //import L from 'leaflet'
-import { LMap, LTileLayer, LMarker, LPopup/*, LTooltip */ } from 'vue2-leaflet'
+import { LMap, LTileLayer, LMarker, LPopup/*, LTooltip */, LTooltip} from 'vue2-leaflet'
 
 // To solve missing icons
 delete L.Icon.Default.prototype._getIconUrl
@@ -148,8 +147,8 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
-    LPopup
-    // LTooltip
+    LPopup,
+    LTooltip
   },
   data () {
     return {
@@ -173,26 +172,34 @@ export default {
         active_name: null,
         active_bind: null,
       showParagraph: true,
+      ClickMarkerIcon: null,
+      iconDefault: L.icon({
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+      }),
       iconSubstation: L.icon({
-        iconUrl: /* 'https://lh3.googleusercontent.com/ADBp5lqoyDlVEH6XtOLw55Bv-HYn4zzNjwY2CGaWd3C565Rbdp_0uOyETZfy4ppV1GdvN1GydR-fxNFYn_rB=w1920-h1008' */ require('../assets/substation.png') ,
-        shadowUrl: /* 'https://lh3.googleusercontent.com/mGCdckEPv5TII-w_j1K1wcLiNUIaWCkHEVVW6tODBL0w94OQKLIE8lyqPI-xTFQ3P0_y1lRMyYAmG1riWanE=w1920-h1008' */ require('../assets/substation_shadow.png'),
+        iconUrl: require('../assets/substation.png') ,
+        shadowUrl: require('../assets/substation_shadow.png'),
         iconSize: [50, 45],
-        iconAnchor: [40, 37],
+        // iconAnchor: [40, 37],
         shadowSize: [50, 45],
-        shadowAnchor: [29, 37]
+        // shadowAnchor: [29, 37],
+         tooltipAnchor: [10,0]
       }),
       iconTransformator: L.icon({
-        iconUrl: /* 'https://lh4.googleusercontent.com/pFHh5jBrMEQzCaaq60KORJewL_UPQ92HK-QPQjmrFwxEUzqPpYyb5uQ1h8Fnoh9Ke4RWImRbWP3Q1A6cILPX=w952-h991' */require('../assets/tranformer.png'),
-        shadowUrl:/* 'https://lh4.googleusercontent.com/vKcAc0AjLbvlA2upY6-SWebeE1GACLO6CJ2nlh-12WcMB_uyCGhChUEpd2QJwIXCTDfjoY56awhebumeWIRZ=w952-h991' */require('../assets/tranformer-shadow.png'),
+        iconUrl: require('../assets/tranformer.png'),
+        shadowUrl: require('../assets/tranformer-shadow.png'),
         iconSize: [50, 45],
-        iconAnchor: [40, 37],
+      //  iconAnchor: [20, 37],
         shadowSize: [50, 45],
-        shadowAnchor: [35, 37]
+       // shadowAnchor: [20, 37],
+        tooltipAnchor: [7,0]
       }),
       iconEMeter: L.icon({
         iconUrl: require('../assets/meter.png'),
-        iconSize: [50, 45],
-        iconAnchor: [40, 37]
+        iconSize: [50, 45]
+        //iconAnchor: [40, 37]
       }),
       mapOptions: {
         zoomSnap: 0.5
@@ -220,10 +227,28 @@ export default {
     },
     selectSite (e) {
       this.positionPopup = e.latlng
+      
+        this.$refs.ClickMarker.setVisible(true)
+     
+      
       /* var nam = String.fromCharCode((Math.random() * 100))
       var sub = { name: nam, latitude: e.latlng.lat, longitude: e.latlng.lng } */
       /* console.log(sub) */
       /* this.register(sub) */
+    },
+    changeMarkerIcon(){
+      console.log(this.active_type)
+      if (this.active_type === 'S') {
+        this.ClickMarkerIcon= this.iconSubstation
+        } 
+      else if(this.active_type === 'T') {
+          this.ClickMarkerIcon= this.iconTransformator
+          } 
+      else if (this.active_type === 'E') {
+        this.ClickMarkerIcon= this.iconEMeter
+        } 
+      else {
+        this.ClickMarkerIcon= L.Marker.prototype.options.icon}
     },
     getActiveNames () {
       var actnames;
@@ -239,9 +264,9 @@ export default {
     },
     register () {
       console.log(this.active_bind)
-      switch (this.active_type) {
+       switch (this.active_type) {
         case 'S':
-          this.active= {name: this.active_name, latitude: this.positionPopup.lat, longitude: this.positionPopup.lng }
+          this.active= {name: this.active_name, latitude: this.positionPopup.lat, longitude: this.positionPopup.lng, is_active: true }
           this.$store.dispatch('registerSubstation', this.active)
         .then(res => {
           alert('sub registered successfully', this.active.name)
@@ -251,7 +276,7 @@ export default {
         })
           break;
         case 'T':
-          this.active= {name: this.active_name, latitude: this.positionPopup.lat, longitude: this.positionPopup.lng, substation: this.active_bind }
+          this.active= {name: this.active_name, latitude: this.positionPopup.lat, longitude: this.positionPopup.lng, substation: this.active_bind, is_active: true}
           console.log(this.active)
           this.$store.dispatch('registerTransformer', this.active)
         .then(res => {
@@ -262,7 +287,7 @@ export default {
         })
           break;
         case 'E':
-          this.active= {name: this.active_name, latitude: this.positionPopup.lat, longitude: this.positionPopup.lng, transformer: this.active_bind }
+          this.active= {name: this.active_name, latitude: this.positionPopup.lat, longitude: this.positionPopup.lng, transformer: this.active_bind, is_active: true }
           this.$store.dispatch('registerMeter', this.active)
         .then(res => {
           alert('meter registered successfully', this.active.name)
@@ -277,12 +302,15 @@ export default {
       } 
     this.$refs.ClickPopupForm.reset()
     this.$refs.ClickPopupForm.resetValidation()
-    // this.$refs.ClickMarker.setVisible(false)
+    this.$refs.ClickMarker.setVisible(false)
+    this.$refs.Map.$forceUpdate()
     this.$store.dispatch('getSubstations')
     this.$store.dispatch('getTransformers')
     this.$store.dispatch('getMeters')
-    /* this.$forceUpdate() */
-    console.log(this.$refs.TranformersMarker)
+
+       
+    //this.positionPopup = L.latLng(this.positionPopup.lat + 0.001,this.positionPopup.lng + 0.0001)
+    this.ClickMarkerIcon = L.Marker.prototype.options.icon
     // this.$forceUpdate
     }
   },
